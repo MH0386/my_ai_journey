a2cc2964cd146aab2d0911614efdff1add1cade7
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-13 20:26:29 +0200

    Disable enterTest section in devenv.nix
    Comment out the enterTest section in devenv.nix.

--- devenv.nix
+++ devenv.nix
@@ -123,10 +123,10 @@
   # };
 
   # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
 
   # https://devenv.sh/git-hooks/
   git-hooks.hooks = {
